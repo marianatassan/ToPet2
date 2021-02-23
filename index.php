a98fdd7034e256356ee7c26df8e8aa50ffff18b5
Mariana Tassan <51093986+marianatassan@users.noreply.github.com>
2021-02-23 10:16:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-  include_once("index.html");
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sistema teste</title>
+</head>
+<body>
+	<?php require_once 'usuariologado.php'; ?>
+</body>
+</html>
